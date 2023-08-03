3f9bad256316cc58e32dbe0e5ae79889138cee62
Emma0147 <mromotoriogunzabere@gmail.com>
2023-08-03 11:19:15 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 """
-Personal data
+Module for handling Personal Data
 """
-import re
 import logging
+import mysql.connector
+from os import environ
+import re
 from typing import List
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ returns the log message obfuscated """
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
     for f in fields:
-        message = re.sub(rf"{f}=(.*?)\{separator}",
+        message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
 
