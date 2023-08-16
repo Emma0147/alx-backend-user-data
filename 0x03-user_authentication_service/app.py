ea2b797eeb74008e5381a0c2e2ba9408e866679d
Emma0147 <mromotoriogunzabere@gmail.com>
2023-08-16 12:30:49 +0000

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,5 +45,20 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    Logout user
+    """
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
