8c35586f30c609223e4d28d613b86f600ec8b143
Emma0147 <mromotoriogunzabere@gmail.com>
2023-08-16 12:38:13 +0000

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect, url_for
 from auth import Auth
 
 app = Flask(__name__)
